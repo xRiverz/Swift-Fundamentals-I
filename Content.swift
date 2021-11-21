a0fdf7495d9b900eeb5dc5f3e35915191ade10fd
Anhar AG <85056292+xRiverz@users.noreply.github.com>
2021-11-21 10:53:38 +0300

    Create Content.swift

---
+++ Content.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var type: String = "Rectangle"
+var descripion: String = "A qu"
+var width: Int = 5
+var height: Int = 10
+var area: Double = Double(width) * Double(height)
+height += 1
+width += 1
+area = Double(width) * Double(height)
+print("The shape is a \(type) or\(descripion) witth an area of \(area)")
