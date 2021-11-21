52eb3bed2d961b8a4d21c3359d3a587a6e18eee7
Anhar AG <85056292+xRiverz@users.noreply.github.com>
2021-11-21 10:59:23 +0300

    Update Content.swift

--- Content.swift
+++ Content.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 var type: String = "Rectangle"
-var descripion: String = "A qu"
+var descripion: String = A quadrilateral with four right angles"
 var width: Int = 5
 var height: Int = 10
 var area: Double = Double(width) * Double(height)
